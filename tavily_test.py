4e746f46c989fd472b321cc06f4caa09bb6d02c6
Sarthak Sharma <sarthak98970@gmail.com>
2026-06-08 06:25:01 -0700

    Initial commit

---
+++ tavily_test.py
@@ -0,0 +1,10 @@
+from tavily import TavilyClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+result = client.search("Artificial Intelligence")
+print(result)
